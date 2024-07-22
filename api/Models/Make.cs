e777e1d72b39b72f7cfe82c41969eddfa468b2cc
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-22 23:08:19 +0800

    added Make class

---
+++ api/Models/Make.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Make
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Model> Models { get; set; }
+        public Make()
+        {
+            Models = new Collection<Model>();
+        }
+    }
+}
