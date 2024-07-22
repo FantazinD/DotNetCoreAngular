5325f466cfb70fe2503f1b624015b243ab5d912c
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-22 23:36:41 +0800

    Initial db migrations

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -4,8 +4,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
-    public class ApplicationDBContext(DbContextOptions dbContextOptions) : DbContext
+    public class ApplicationDBContext : DbContext
     {
-        
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+            
+        }
     }
 }
