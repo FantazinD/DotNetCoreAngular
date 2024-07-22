3cbe0afadd87f65ed173eb1b8373e184b107d0f1
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-22 23:10:41 +0800

    updated Model class in api

--- api/Models/Model.cs
+++ api/Models/Model.cs
@@ -9,5 +9,7 @@ namespace api.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public Make Make { get; set; }
+        public int MakeId { get; set; }
     }
 }
