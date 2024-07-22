2d95981cb0af16cf67abe5a1cd681f9b1da455de
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-23 05:07:28 +0800

    added Contraints to Models

--- api/Models/Make.cs
+++ api/Models/Make.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace api.Models
     public class Make
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public ICollection<Model> Models { get; set; }
         public Make()
