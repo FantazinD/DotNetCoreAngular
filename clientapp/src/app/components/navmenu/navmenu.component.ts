7bc11225c77b9f2d3f3cbc1655e6ac32773f4fa1
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-24 23:20:38 +0800

    added scaffolding for navmenu component

---
+++ clientapp/src/app/components/navmenu/navmenu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navmenu',
+  standalone: true,
+  imports: [],
+  templateUrl: './navmenu.component.html',
+  styleUrl: './navmenu.component.css'
+})
+export class NavmenuComponent {
+
+}
