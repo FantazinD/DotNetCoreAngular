e8afaf907ae66bde32efd848e86dd2493193d62a
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-25 09:06:59 +0800

    added scaffolding for fetchdata component

---
+++ clientapp/src/app/components/fetchdata/fetchdata.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fetchdata',
+  standalone: true,
+  imports: [],
+  templateUrl: './fetchdata.component.html',
+  styleUrl: './fetchdata.component.css'
+})
+export class FetchdataComponent {
+
+}
