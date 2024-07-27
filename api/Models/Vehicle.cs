73fb04ef210561ba95ca31b05b234e9524dec72b
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-27 20:32:06 +0800

    updated Tablename of Vehicle

--- api/Models/Vehicle.cs
+++ api/Models/Vehicle.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace api.Models
 {
+    [Table("Vehicles")]
     public class Vehicle
     {
         public int Id { get; set; }
