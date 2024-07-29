430e44eb73340eff787b1423e2fe026cd666ba7a
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-30 07:01:15 +0800

    angular property binding

--- clientapp/src/app/app.component.ts
+++ clientapp/src/app/app.component.ts
@@ -10,5 +10,7 @@ import { HomeComponent } from './components/home/home.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Vega';
+  title: string = 'Vega';
+  imgUrl: string =
+    'https://yt3.googleusercontent.com/y4J_Fs5ksRlxx6_LzT1VKxVqH_T8Vyn_RN_YYgLJhuMzBS5qxTgm7NlEcMkQd3hgCpfWtYcEUg=s900-c-k-c0x00ffffff-no-rj';
 }
