efb87eb1290fb5849897f1c81a03d004aee9919f
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-07-29 09:40:54 +0800

    exploring angular standalone components

--- clientapp/src/app/app.component.ts
+++ clientapp/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavmenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'clientapp';
+  title = 'Vega';
 }
