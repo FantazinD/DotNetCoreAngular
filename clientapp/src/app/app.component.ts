b12f305a81bcb36963361e57855a9514b3347fcf
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-08-03 12:56:07 +0800

    style binding with ternary condition

--- clientapp/src/app/app.component.ts
+++ clientapp/src/app/app.component.ts
@@ -14,6 +14,6 @@ export class AppComponent {
   imgUrl: string =
     'https://yt3.googleusercontent.com/y4J_Fs5ksRlxx6_LzT1VKxVqH_T8Vyn_RN_YYgLJhuMzBS5qxTgm7NlEcMkQd3hgCpfWtYcEUg=s900-c-k-c0x00ffffff-no-rj';
   isDisabled: boolean = true;
-  isActive: boolean = true;
+  isActive: boolean = false;
   fruitName: string = 'Apple';
 }
