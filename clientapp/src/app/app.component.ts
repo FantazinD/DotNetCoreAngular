8840ecc23dacebd29653be82550f0627307dea93
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-08-05 19:49:40 +0800

    angular built-in two-way data binding

--- clientapp/src/app/app.component.ts
+++ clientapp/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
