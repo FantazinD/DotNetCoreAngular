b7054889d2be40a7bf4f32da617eee6fbfd26e1c
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-08-14 01:12:13 +0800

    added 'about' to Routes

--- clientapp/src/app/app.routes.ts
+++ clientapp/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
 
-export const routes: Routes = [{ path: '', component: HomeComponent }];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: NavmenuComponent },
+];
