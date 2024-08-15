edfd400da14ed1ce423e426b8b158cc949642459
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-08-16 00:33:11 +0800

    added new route for vehicle form

--- clientapp/src/app/app.routes.ts
+++ clientapp/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { NavmenuComponent } from './components/navmenu/navmenu.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: NavmenuComponent },
-  { path: 'contact', component: HomeComponent },
+  { path: 'vehicles/new', component: AboutComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
 ];
