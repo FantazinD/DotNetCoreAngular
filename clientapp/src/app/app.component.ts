c9822e71a99ae713250f81d46fe54670e95411a7
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-08-19 22:28:39 +0800

    restarted app component.html

--- clientapp/src/app/app.component.ts
+++ clientapp/src/app/app.component.ts
@@ -16,49 +16,4 @@ import { NavmenuComponent } from './components/navmenu/navmenu.component';
 })
 export class AppComponent {
   title: string = 'Vega';
-  imgUrl: string =
-    'https://yt3.googleusercontent.com/y4J_Fs5ksRlxx6_LzT1VKxVqH_T8Vyn_RN_YYgLJhuMzBS5qxTgm7NlEcMkQd3hgCpfWtYcEUg=s900-c-k-c0x00ffffff-no-rj';
-  isDisabled: boolean = true;
-  isActive: boolean = true;
-  fruitName: string = 'Apple';
-  isLoggedIn: boolean = true;
-  userName: string = 'John Doe';
-  buttonClick = () => {
-    console.log('hehe');
-  };
-  keyEnter = (event: any) => {
-    if (event.keyCode == 13) {
-      console.log('Enter Key Pressed.');
-    }
-  };
-  keyupFiltering = (user: HTMLInputElement) => {
-    console.log(user.id);
-  };
-  updateUsername = (username: HTMLInputElement) => {
-    this.userName = username.value;
-    console.log(this.userName);
-  };
-  loginAttempts: number = 0;
-  countLoginAttempts = () => {
-    this.loginAttempts++;
-  };
-  userRole: string = 'TestRole';
-  users: Array<string> = ['John', 'Sam', 'Smith', 'Heel'];
-  usersObj: Array<any> = [
-    { Name: 'John', Id: 1 },
-    { Name: 'Sam', Id: 2 },
-    { Name: 'Smith', Id: 3 },
-    { Name: 'Heel', Id: 4 },
-  ];
-  addToUserObjArray = (users: any[]) => {
-    let user: any = { Name: `Hwapo ${users.length}`, Id: users.length + 1 };
-    this.usersObj.push(user);
-  };
-  deleteUserFromObjArray = (userId: number) => {
-    let newUsers: Array<any> = this.usersObj.filter(
-      (userObj) => userObj.Id !== userId
-    );
-
-    this.usersObj = newUsers;
-  };
 }
