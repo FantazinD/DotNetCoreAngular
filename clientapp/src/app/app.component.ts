7b611ac0f914893d8939654fd31146fd1bbc2d4f
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-08-19 23:31:39 +0800

    imported bootstrap to styles.css

--- clientapp/src/app/app.component.ts
+++ clientapp/src/app/app.component.ts
@@ -9,10 +9,7 @@ import { NavmenuComponent } from './components/navmenu/navmenu.component';
   standalone: true,
   imports: [RouterOutlet, HomeComponent, NavmenuComponent, FormsModule],
   templateUrl: './app.component.html',
-  styleUrls: [
-    './app.component.css',
-    '../../node_modules/bootstrap/dist/css/bootstrap.min.css',
-  ],
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title: string = 'Vega';
