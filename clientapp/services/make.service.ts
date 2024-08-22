2de4d490b38795bac7e0bf30885ef12836ec15e0
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-08-22 16:29:38 +0800

    added getMakes to make service

--- clientapp/services/make.service.ts
+++ clientapp/services/make.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MakeService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getMakes() {
+    return this.http.get('/api/makes').pipe(map((res: any) => res.json()));
+  }
 }
