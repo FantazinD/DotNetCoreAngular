5fd8763f3c758222a3ad850a7bde10e18c0823ad
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-08-23 04:40:17 +0800

    populated Make dropdown with data from api

--- clientapp/src/app/components/vehicle-form/vehicle-form.component.ts
+++ clientapp/src/app/components/vehicle-form/vehicle-form.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { MakeService } from './../../../../services/make.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-vehicle-form',
   standalone: true,
   imports: [],
   templateUrl: './vehicle-form.component.html',
-  styleUrl: './vehicle-form.component.css'
+  styleUrl: './vehicle-form.component.css',
 })
-export class VehicleFormComponent {
+export class VehicleFormComponent implements OnInit {
+  makes: any;
+  constructor(private makeService: MakeService) {}
 
+  ngOnInit(): void {
+    this.makeService.getMakes().subscribe((makes: any) => {
+      this.makes = makes;
+    });
+  }
 }
