e5877ff3aa6939d867d679cb24d474e3ac905ac5
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-08-24 23:24:31 +0800

    bugfix: data from model dropdown is retained when make dropdown is blank

--- clientapp/src/app/components/vehicle-form/vehicle-form.component.ts
+++ clientapp/src/app/components/vehicle-form/vehicle-form.component.ts
@@ -23,6 +23,6 @@ export class VehicleFormComponent implements OnInit {
 
   onMakeChange = (): void => {
     let selectedMake = this.makes.find((make) => make.id == this.vehicle.make);
-    this.models = selectedMake.models;
+    this.models = selectedMake ? selectedMake.models : [];
   };
 }
