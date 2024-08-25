2de733e1efbeee9726df6dfc118ff5e5ca8e989c
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-08-26 04:39:59 +0800

    added VehicleFeature model

--- api/Models/Vehicle.cs
+++ api/Models/Vehicle.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -25,5 +26,10 @@ namespace api.Models
         [StringLength(255)]
         public string ContactPhone { get; set; } = string.Empty;
         public DateTime LastUpdate { get; set; }
+        public ICollection<VehicleFeature> Features { get; set; }
+        public Vehicle()
+        {
+            Features = new Collection<VehicleFeature>();
+        }
     }
 }
