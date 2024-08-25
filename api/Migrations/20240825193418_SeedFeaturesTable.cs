42a8f99fe47886d17f61957c225cc4e9b2710896
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-08-26 03:35:48 +0800

    seeded Features Table using codefirst migration

---
+++ api/Migrations/20240825193418_SeedFeaturesTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedFeaturesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Feature1')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Feature2')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES ('Feature3')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Features WHERE Name IN ('Feature1', 'Feature2', 'Feature3')");
+        }
+    }
+}
