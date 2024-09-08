9b6a6162ebed1b4d61041c708de69dd88cde88c6
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-09-08 23:29:11 +0800

    service now gets from api instead of dummy data

--- clientapp/services/vehicle.service.ts
+++ clientapp/services/vehicle.service.ts
@@ -9,87 +9,87 @@ export class VehicleService {
   constructor(private http: HttpClient) {}
 
   getMakes() {
-    let arr: any[] = [
-      {
-        id: 1,
-        name: 'Make1',
-        models: [
-          {
-            id: 1,
-            name: 'Make1-ModelA',
-          },
-          {
-            id: 2,
-            name: 'Make1-ModelB',
-          },
-          {
-            id: 3,
-            name: 'Make1-ModelC',
-          },
-        ],
-      },
-      {
-        id: 2,
-        name: 'Make2',
-        models: [
-          {
-            id: 4,
-            name: 'Make2-ModelA',
-          },
-          {
-            id: 5,
-            name: 'Make2-ModelB',
-          },
-          {
-            id: 6,
-            name: 'Make2-ModelC',
-          },
-        ],
-      },
-      {
-        id: 3,
-        name: 'Make3',
-        models: [
-          {
-            id: 7,
-            name: 'Make3-ModelA',
-          },
-          {
-            id: 8,
-            name: 'Make3-ModelB',
-          },
-          {
-            id: 9,
-            name: 'Make3-ModelC',
-          },
-        ],
-      },
-    ];
-    return arr;
-    // return this.http
-    //   .get('http://localhost:5166/api/makes')
-    //   .pipe(map((res) => res));
+    // let arr: any[] = [
+    //   {
+    //     id: 1,
+    //     name: 'Make1',
+    //     models: [
+    //       {
+    //         id: 1,
+    //         name: 'Make1-ModelA',
+    //       },
+    //       {
+    //         id: 2,
+    //         name: 'Make1-ModelB',
+    //       },
+    //       {
+    //         id: 3,
+    //         name: 'Make1-ModelC',
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     id: 2,
+    //     name: 'Make2',
+    //     models: [
+    //       {
+    //         id: 4,
+    //         name: 'Make2-ModelA',
+    //       },
+    //       {
+    //         id: 5,
+    //         name: 'Make2-ModelB',
+    //       },
+    //       {
+    //         id: 6,
+    //         name: 'Make2-ModelC',
+    //       },
+    //     ],
+    //   },
+    //   {
+    //     id: 3,
+    //     name: 'Make3',
+    //     models: [
+    //       {
+    //         id: 7,
+    //         name: 'Make3-ModelA',
+    //       },
+    //       {
+    //         id: 8,
+    //         name: 'Make3-ModelB',
+    //       },
+    //       {
+    //         id: 9,
+    //         name: 'Make3-ModelC',
+    //       },
+    //     ],
+    //   },
+    // ];
+    // return arr;
+    return this.http
+      .get('http://localhost:5166/api/makes')
+      .pipe(map((res) => res));
   }
 
   getFeatures() {
-    let arr: any[] = [
-      {
-        id: 1,
-        name: 'Feature1',
-      },
-      {
-        id: 2,
-        name: 'Feature2',
-      },
-      {
-        id: 3,
-        name: 'Feature3',
-      },
-    ];
+    // let arr: any[] = [
+    //   {
+    //     id: 1,
+    //     name: 'Feature1',
+    //   },
+    //   {
+    //     id: 2,
+    //     name: 'Feature2',
+    //   },
+    //   {
+    //     id: 3,
+    //     name: 'Feature3',
+    //   },
+    // ];
+    // return arr;
 
-    return arr;
-    // return this.http
-    //   .get('http://localhost:5166/api/features')
-    //   .pipe(map((res) => res));
+    return this.http
+      .get('http://localhost:5166/api/features')
+      .pipe(map((res) => res));
   }
 }
