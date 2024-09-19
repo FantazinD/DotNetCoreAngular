60312592aa9549f95c4eda6a5c87e987fe0593d5
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-09-19 12:03:06 +0800

    implemented client-side pagination

--- clientapp/src/app/components/shared/pagination/pagination.component.ts
+++ clientapp/src/app/components/shared/pagination/pagination.component.ts
@@ -24,30 +24,33 @@ export class PaginationComponent implements OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
     this.currentPage = 1;
 
-    var pagesCount = Math.ceil(this.totalItems / this.pageSize);
+    let pagesCount = Math.ceil(this.totalItems / this.pageSize);
     this.pages = [];
-    for (var i = 1; i <= pagesCount; i++) this.pages.push(i);
+    for (let i = 1; i <= pagesCount; i++) this.pages.push(i);
 
+    console.log('mada');
     console.log(this);
   }
 
-  changePage = (page: number) => {
+  onChangePage = (page: number) => {
     this.currentPage = page;
+
     this.pageChanged.emit(page);
   };
 
-  previous = () => {
+  onClickPrevious = () => {
     if (this.currentPage == 1) return;
 
     this.currentPage--;
+
     this.pageChanged.emit(this.currentPage);
   };
 
-  next = () => {
+  onClickNext = () => {
     if (this.currentPage == this.pages.length) return;
 
     this.currentPage++;
-    console.log('next', this);
+
     this.pageChanged.emit(this.currentPage);
   };
 }
--- clientapp/src/app/components/vehicle-list/vehicle-list.component.ts
+++ clientapp/src/app/components/vehicle-list/vehicle-list.component.ts
@@ -4,18 +4,21 @@ import { VehicleService } from '../../../../services/vehicle.service';
 import { RouterModule } from '@angular/router';
 import { IKeyValuePair } from '../../interfaces/IKeyValuePair';
 import { FormsModule } from '@angular/forms';
+import { PaginationComponent } from '../shared/pagination/pagination.component';
 
 @Component({
   selector: 'app-vehicle-list',
   standalone: true,
-  imports: [RouterModule, FormsModule],
+  imports: [RouterModule, FormsModule, PaginationComponent],
   templateUrl: './vehicle-list.component.html',
   styleUrl: './vehicle-list.component.css',
 })
 export class VehicleListComponent implements OnInit {
   vehicles: IVehicle[] = [];
   makes: IKeyValuePair[] = [];
-  query: any = {};
+  query: any = {
+    pageSize: 2,
+  };
   columns: any[] = [
     { title: 'Id' },
     { title: 'Make', key: 'make', isSortable: true },
@@ -64,6 +67,11 @@ export class VehicleListComponent implements OnInit {
       this.query.sortBy = columnName;
       this.query.isSortAscending = true;
     }
+    this.populateVehicles();
+  };
+
+  onPageChange = (page: number) => {
+    this.query.page = page;
     this.populateVehicles();
   };
 }
