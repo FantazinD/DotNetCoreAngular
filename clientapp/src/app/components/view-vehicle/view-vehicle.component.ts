cd281247545ae42cba3c27fff14039de6b9b2327
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-09-21 19:09:42 +0800

    completed view-vehicle nav behavior

--- clientapp/src/app/components/view-vehicle/view-vehicle.component.ts
+++ clientapp/src/app/components/view-vehicle/view-vehicle.component.ts
@@ -1,12 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { VehicleService } from '../../../../services/vehicle.service';
+import { ToastrService } from 'ngx-toastr';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-view-vehicle',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, NgbNavModule],
   templateUrl: './view-vehicle.component.html',
-  styleUrl: './view-vehicle.component.css'
+  styleUrl: './view-vehicle.component.css',
 })
-export class ViewVehicleComponent {
+export class ViewVehicleComponent implements OnInit {
+  active = 1;
+  vehicle: any;
+  vehicleId: number = 0;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastrService: ToastrService,
+    private vehicleService: VehicleService
+  ) {
+    route.params.subscribe((p) => {
+      this.vehicleId = +p['id'];
+      if (isNaN(this.vehicleId) || this.vehicleId <= 0) {
+        router.navigate(['/vehicles']);
+        return;
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.vehicleService.getVehicle(this.vehicleId).subscribe({
+      next: (vehicle) => (this.vehicle = vehicle),
+      error: (err) => {
+        if (err.status == 404) {
+          this.router.navigate(['/vehicles']);
+          return;
+        }
+      },
+    });
+  }
+
+  onDelete() {
+    if (confirm('Are you sure?')) {
+      this.vehicleService.deleteVehicle(this.vehicle.id).subscribe((x) => {
+        this.router.navigate(['/vehicles']);
+      });
+    }
+  }
 }
