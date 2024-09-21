4d6f8d3c09fb0e2d13248276d8d115664248dc38
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-09-21 18:21:14 +0800

    added view-vehicle component to clientapp

--- clientapp/src/app/app.routes.ts
+++ clientapp/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 import { VehicleListComponent } from './components/vehicle-list/vehicle-list.component';
+import { ViewVehicleComponent } from './components/view-vehicle/view-vehicle.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'vehicles/new', component: VehicleFormComponent },
-  { path: 'vehicles/:id', component: VehicleFormComponent },
+  { path: 'vehicles/edit/:id', component: VehicleFormComponent },
+  { path: 'vehicles/:id', component: ViewVehicleComponent },
   { path: 'vehicles', component: VehicleListComponent },
   { path: 'home', component: HomeComponent },
 ];
---
+++ clientapp/src/app/components/view-vehicle/view-vehicle.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-vehicle',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-vehicle.component.html',
+  styleUrl: './view-vehicle.component.css'
+})
+export class ViewVehicleComponent {
+
+}
