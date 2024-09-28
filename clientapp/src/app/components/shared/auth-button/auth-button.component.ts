74edd7362d2cfdcef2f7f473a0007b36b8ed372b
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-09-28 17:58:59 +0800

    added auth-button component

---
+++ clientapp/src/app/components/shared/auth-button/auth-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-button.component.html',
+  styleUrl: './auth-button.component.css'
+})
+export class AuthButtonComponent {
+
+}
