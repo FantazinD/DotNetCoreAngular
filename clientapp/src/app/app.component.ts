5b46bfd926e8a1811ec4080aa66c86135c12486d
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-09-29 04:50:38 +0800

    added spinner on app component to handle authentication process

--- clientapp/src/app/app.component.ts
+++ clientapp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ErrorHandler } from '@angular/core';
+import { Component, ErrorHandler, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { FormsModule } from '@angular/forms';
@@ -6,11 +6,21 @@ import { NavmenuComponent } from './components/navmenu/navmenu.component';
 import { VehicleService } from '../../services/vehicle.service';
 import { AppErrorHandler } from './app.error-handler';
 import { PhotoService } from '../../services/photo.service';
+import { AuthService } from '@auth0/auth0-angular';
+import { CommonModule } from '@angular/common';
+import { LoadingComponent } from './components/shared/loading/loading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, NavmenuComponent, FormsModule],
+  imports: [
+    RouterOutlet,
+    HomeComponent,
+    NavmenuComponent,
+    FormsModule,
+    CommonModule,
+    LoadingComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [
@@ -19,6 +29,10 @@ import { PhotoService } from '../../services/photo.service';
     PhotoService,
   ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = 'Vega';
+
+  constructor(public auth: AuthService) {}
+
+  ngOnInit(): void {}
 }
