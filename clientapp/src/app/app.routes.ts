76163685d50475a1d625d6ceb4591ffce2cbcf79
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-09-30 10:41:43 +0800

    added admin component

--- clientapp/src/app/app.routes.ts
+++ clientapp/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './components/home/home.component';
 import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 import { VehicleListComponent } from './components/vehicle-list/vehicle-list.component';
 import { ViewVehicleComponent } from './components/view-vehicle/view-vehicle.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'vehicles/edit/:id', component: VehicleFormComponent },
   { path: 'vehicles/:id', component: ViewVehicleComponent },
   { path: 'vehicles', component: VehicleListComponent },
+  { path: 'admin', component: AdminComponent },
   { path: 'home', component: HomeComponent },
 ];
