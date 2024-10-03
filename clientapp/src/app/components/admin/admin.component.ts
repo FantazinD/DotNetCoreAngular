56581cbe9cbdba3218d81106e400924e016b8b30
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-10-04 02:34:55 +0800

    added dummy pie chart on admin page

--- clientapp/src/app/components/admin/admin.component.ts
+++ clientapp/src/app/components/admin/admin.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { ChartOptions } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css',
 })
-export class AdminComponent {}
+export class AdminComponent {
+  // Pie
+  public pieChartOptions: ChartOptions<'pie'> = {
+    responsive: false,
+  };
+  public pieChartLabels = [
+    ['Download', 'Sales'],
+    ['In', 'Store', 'Sales'],
+    'Mail Sales',
+  ];
+  public pieChartDatasets = [
+    {
+      data: [300, 500, 100],
+    },
+  ];
+  public pieChartLegend = true;
+  public pieChartPlugins = [];
+}
