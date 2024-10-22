aa94ae7c870020c79f106221f1cebf2f2d1b710d
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-10-23 03:24:27 +0800

    added routerLink to Browse button in HomeComponent

--- clientapp/src/app/components/home/home.component.ts
+++ clientapp/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavmenuComponent } from '../navmenu/navmenu.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
