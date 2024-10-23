18f9695952a451c1d9bf3072de2343f554d35514
Fantazindy <paulo.pete.k.razon@gmail.com>
2024-10-24 03:36:36 +0800

    added tailwind config and added font-family styling

--- clientapp/tailwind.config.ts
+++ clientapp/tailwind.config.ts
@@ -2,10 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
+    extend: {
+      colors: {
+        'burnt-orange': '#e66000',
+        'bright-orange': '#ff9500',
+      },
+    },
     container: {
       center: true,
     },
-    extend: {},
   },
   plugins: [],
 };
