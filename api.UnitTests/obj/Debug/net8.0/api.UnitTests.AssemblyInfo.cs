e6d934cb6018d3108bea2cc654b208babff0dd18
Paulo-WinPC-Punta <paulo.pete.k.razon@gmail.com>
2025-01-14 14:18:40 +0800

    added Moq package to UnitTest project

--- api.UnitTests/obj/Debug/net8.0/api.UnitTests.AssemblyInfo.cs
+++ api.UnitTests/obj/Debug/net8.0/api.UnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d310ea8e9c170ccd12017dfe2efd4cc4b8d4ecd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba859e988dd50f397d800f7555774f095bcdcb42")]
 [assembly: System.Reflection.AssemblyProductAttribute("api.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
