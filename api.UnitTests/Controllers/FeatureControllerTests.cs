ec7ef155e306938f963cf5195e818b67faf2bfc5
Paulo-WinPC-Punta <paulo.pete.k.razon@gmail.com>
2025-01-14 15:04:05 +0800

    added FeatureControllerTests

---
+++ api.UnitTests/Controllers/FeatureControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using api.Controllers;
+using api.Interfaces;
+using api.Models;
+
+namespace api.UnitTests.Controllers
+{
+    public class FeatureControllerTests
+    {
+        private Mock<IFeatureRepository> _featureRepository;
+        private FeatureController _featureController;
+
+        [SetUp]
+        public void Setup()
+        {
+            _featureRepository = new Mock<IFeatureRepository>();
+            _featureRepository.Setup(fr => fr.GetFeaturesAsync()).ReturnsAsync(new List<Feature>());
+
+            _featureController = new FeatureController(_featureRepository.Object);
+        }
+
+        [Test]
+        public void GetFeatures_WhenCalled_GetsFeatures()
+        {
+            _featureController.GetFeatures().Wait();
+
+            _featureRepository.Verify(fr => fr.GetFeaturesAsync());
+        }
+    }
+}
