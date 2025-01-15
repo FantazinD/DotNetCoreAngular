37e4e042d807099a3e92edcce7d2ef219ea9d859
Paulo-WinPC-Punta <paulo.pete.k.razon@gmail.com>
2025-01-16 01:09:35 +0800

    added GetPhotos interaction test

--- api.UnitTests/Controllers/PhotoControllerTests.cs
+++ api.UnitTests/Controllers/PhotoControllerTests.cs
@@ -22,15 +22,22 @@ namespace api.UnitTests.Controllers
         private Mock<IVehicleRepository> _vehicleRepository;
         private Mock<IPhotoRepository> _photoRepository;
         private Mock<IPhotoService> _photoService;
+        private Vehicle _vehicle;
         private PhotoController _photoController;
 
         [SetUp]
         public void SetUp()
         {
+            _vehicle = new Vehicle()
+            {
+                Id = 1
+            };
             _host = new Mock<IWebHostEnvironment>();
             _vehicleRepository = new Mock<IVehicleRepository>();
             _photoSettings = new Mock<IOptionsSnapshot<PhotoSetting>>();
             _photoRepository = new Mock<IPhotoRepository>();
+            _photoRepository.Setup(pr => pr.GetPhotos(_vehicle.Id)).ReturnsAsync(new List<Photo>());
+
             _photoService = new Mock<IPhotoService>();
 
             _photoController = new PhotoController(
@@ -39,6 +46,14 @@ namespace api.UnitTests.Controllers
                 _photoSettings.Object, 
                 _photoRepository.Object, 
                 _photoService.Object);
+        }
+
+        [Test]
+        public void GetPhotos_WhenCalled_GetsPhotosFromDatabase()
+        {
+            _photoController.GetPhotos(_vehicle.Id).Wait();
+
+            _photoRepository.Verify(pr => pr.GetPhotos(_vehicle.Id));
         }
 
         [Test]
