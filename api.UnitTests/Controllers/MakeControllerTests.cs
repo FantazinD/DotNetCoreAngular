ca24c78ed8005136b4aa76162398cf5f7d1a0da5
Paulo-WinPC-Punta <paulo.pete.k.razon@gmail.com>
2025-01-21 01:29:00 +0800

    added MakeControllerTests and SetUp

---
+++ api.UnitTests/Controllers/MakeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using api.Controllers;
+using api.Interfaces;
+using api.Models;
+
+namespace api.UnitTests;
+
+public class MakeControllerTests
+{
+    private Mock<IFeatureRepository> _featureRepository;
+    private FeatureController _featureController;
+
+    [SetUp]
+    public void Setup()
+    {
+        _featureRepository = new Mock<IFeatureRepository>();
+        _featureRepository.Setup(fr => fr.GetFeaturesAsync()).ReturnsAsync(new List<Feature>());
+
+        _featureController = new FeatureController(_featureRepository.Object);
+    }
+}
