821d423e93db9ffac2f352a051d10cc3d1659bec
Paulo-WinPC-Punta <paulo.pete.k.razon@gmail.com>
2025-01-22 02:38:22 +0800

    added VehicleControllerTests and initial SetUp

---
+++ api.UnitTests/Controllers/VehicleControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using api.Controllers;
+using api.Interfaces;
+
+namespace api.UnitTests.Controllers
+{
+    public class VehicleControllerTests
+    {
+        private Mock<IVehicleRepository> _vehicleRepository;
+        private Mock<IUnitOfWorkRepository> _unitOfWorkRepository;
+        private VehicleController _vehicleController;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _vehicleRepository = new Mock<IVehicleRepository>();
+            _unitOfWorkRepository = new Mock<IUnitOfWorkRepository>();
+
+            _vehicleController = new VehicleController(_vehicleRepository.Object, _unitOfWorkRepository.Object);
+        }
+    }
+}
