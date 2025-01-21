597a022cb2f57c5b3c9b46ebd1680ce26b92bc47
Paulo-WinPC-Punta <paulo.pete.k.razon@gmail.com>
2025-01-22 02:45:40 +0800

    GetVehicle - added Vehicle Is Null Test Case

--- api.UnitTests/Controllers/VehicleControllerTests.cs
+++ api.UnitTests/Controllers/VehicleControllerTests.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using api.Controllers;
 using api.Interfaces;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace api.UnitTests.Controllers
 {
@@ -13,14 +15,30 @@ namespace api.UnitTests.Controllers
         private Mock<IVehicleRepository> _vehicleRepository;
         private Mock<IUnitOfWorkRepository> _unitOfWorkRepository;
         private VehicleController _vehicleController;
+        private Vehicle _vehicle;
 
         [SetUp]
         public void SetUp()
         {
+            _vehicle = new Vehicle()
+            {
+                Id = 1,
+            };
+
             _vehicleRepository = new Mock<IVehicleRepository>();
             _unitOfWorkRepository = new Mock<IUnitOfWorkRepository>();
 
             _vehicleController = new VehicleController(_vehicleRepository.Object, _unitOfWorkRepository.Object);
+        }
+
+        [Test]
+        public async Task GetVehicle_VehicleIsNull_ReturnNotFoundResult()
+        {
+            _vehicleRepository.Setup(vr => vr.GetVehicleAsync(_vehicle.Id, true)).ReturnsAsync(() => null);
+
+            var result = await _vehicleController.GetVehicle(_vehicle.Id);
+
+            Assert.That(result, Is.TypeOf<NotFoundResult>());
         }
     }
 }
