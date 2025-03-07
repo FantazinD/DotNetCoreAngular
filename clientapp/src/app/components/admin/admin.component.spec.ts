c7e990962cdfcad79c629412b9e3855be94103d8
Paulo-WinPC-Punta <paulo.pete.k.razon@gmail.com>
2025-03-08 01:43:45 +0800

    rearrange code

--- clientapp/src/app/components/admin/admin.component.spec.ts
+++ clientapp/src/app/components/admin/admin.component.spec.ts
@@ -2,8 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AdminComponent } from './admin.component';
 
 describe('AdminComponent', () => {
-  let component: AdminComponent;
   let fixture: ComponentFixture<AdminComponent>;
+  let component: AdminComponent;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
