c72866d7d4003d3dfce917d70c3414063599804c
Paulo-WinPC-Punta <paulo.pete.k.razon@gmail.com>
2025-03-31 22:58:17 +0800

    rearranged imports

--- clientapp/src/app/app.routes.ts
+++ clientapp/src/app/app.routes.ts
@@ -1,11 +1,11 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
 import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 import { VehicleListComponent } from './components/vehicle-list/vehicle-list.component';
 import { ViewVehicleComponent } from './components/view-vehicle/view-vehicle.component';
-import { AdminComponent } from './components/admin/admin.component';
 import { AdminAuthGuardService } from '../../services/admin-auth-guard.service';
+import { AdminComponent } from './components/admin/admin.component';
+import { HomeComponent } from './components/home/home.component';
 import { AuthGuard } from '@auth0/auth0-angular';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
